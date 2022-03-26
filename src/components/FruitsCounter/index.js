5776d4e601a3f3f32dba666be64a45f940e19799
rajeevsagar <rajeev22335@gmail.com>
2022-03-26 11:54:53 +0000

    comments added

--- src/components/FruitsCounter/index.js
+++ src/components/FruitsCounter/index.js
@@ -3,20 +3,28 @@ import {Component} from 'react'
 import './index.css'
 
 class FruitsCounter extends Component {
-  state = {bananasCount: 5, mangoesCount: 4}
+  //  FIX1: Initialize the state with values as below
+  state = {
+    mangoesCount: 4,
+    bananasCount: 5,
+  }
 
+  //  FIX2: Class methods should be defined using arrow functions
   onClickEatBanana = () => {
     this.setState(prevState => ({bananasCount: prevState.bananasCount + 1}))
   }
 
+  //  FIX3: Class methods should be defined using arrow functions
   onClickEatMango = () => {
     this.setState(prevState => ({mangoesCount: prevState.mangoesCount + 1}))
   }
 
   render() {
+    //  FIX4: The state should be accessed using "this"
     const {mangoesCount, bananasCount} = this.state
 
     return (
+      // FIX5: When assigning css class to HTML element in JSX, we have to use the attribute as "className"
       <div className="app-container">
         <div className="fruits-counter">
           <h1 className="count-text">
@@ -27,6 +35,7 @@ class FruitsCounter extends Component {
             <div className="counter-control">
               <img
                 src="https://assets.ccbp.in/frontend/react-js/mango-img.png"
+                // FIX6: The alt attribute value of this image should be "mango"
                 alt="mango"
                 className="fruit-image"
               />
@@ -34,6 +43,7 @@ class FruitsCounter extends Component {
                 <button
                   type="button"
                   className="button"
+                  //   FIX7: The event handler should be passed as a reference to the onClick attribute
                   onClick={this.onClickEatMango}
                 >
                   Eat Mango
@@ -50,6 +60,7 @@ class FruitsCounter extends Component {
                 <button
                   type="button"
                   className="button"
+                  //   FIX8: The event handler should be passed as a reference to the onClick attribute
                   onClick={this.onClickEatBanana}
                 >
                   Eat Banana
